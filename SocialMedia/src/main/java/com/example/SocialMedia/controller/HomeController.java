b83a6716b24e29e509380cf5cd3933e7b1a90e2d
FrankoKaloshi1 <fkaloshi22@epoka.edu.al>
2026-03-02 15:05:56 +0100

    Add HomeController with a simple API endpoint and include spring-boot-starter-web dependency

---
+++ SocialMedia/src/main/java/com/example/SocialMedia/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.SocialMedia.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "SocialMedia API is running!";
+    }
+}
